81dbd85e3d3299da3f5377a9d4f99837c3397b4a
Beytullah KALAY <beytullah.kalay@outlook.com>
2024-03-21 16:13:53 +0300

    modified: scene

--- Assets/_Scripts/DayAndNightController.cs
+++ Assets/_Scripts/DayAndNightController.cs
@@ -6,60 +6,60 @@ using UnityEngine.Rendering.Universal;
 
 public class DayAndNightController : MonoBehaviour
 {
-    public static DayAndNightController Instance;
+	public static DayAndNightController Instance;
 
-    public bool IsNight { get; private set; } = false;
+	public bool IsNight { get; private set; } = false;
 
-    [SerializeField] private float switchNightProgressDuration = 3;
-    [SerializeField] private Color seaNightColor;
-    [SerializeField] private Volume volume;
+	[SerializeField] private float switchNightProgressDuration = 3;
+	[SerializeField] private Color seaNightColor;
+	[SerializeField] private Volume nighVolume;
 
-    private void Awake()
-    {
-        if (Instance == null)
-        {
-            Instance = this;
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
-    }
+	private void Awake()
+	{
+		if (Instance == null)
+		{
+			Instance = this;
+		}
+		else
+		{
+			Destroy(gameObject);
+		}
+	}
 
 
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            var seq = DOTween.Sequence();
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			var seq = DOTween.Sequence();
 
-            seq.Append
-            (
-                DOVirtual.Float(0, 1, switchNightProgressDuration, t =>
-                {
-                    volume.weight = t;
-                    
-                    
-                    if (!IsNight && t >= .85f)
-                    {
-                        IsNight = true;
-                        var lights = GameObject.FindGameObjectsWithTag("Light");
-                        foreach (var l in lights)
-                        {
-                            if (l.TryGetComponent<Light2D>(out var lightComponent))
-                            {
-                                lightComponent.enabled = true;
-                            }
-                        }
-                    }
-                })
-            );
+			seq.Append
+			(
+				DOVirtual.Float(0, 1, switchNightProgressDuration, t =>
+				{
+					nighVolume.weight = t;
 
-            seq.Join
-            (
-                DOVirtual.Color(Camera.main.backgroundColor, seaNightColor, switchNightProgressDuration,
-                    t => { Camera.main.backgroundColor = t; })
-            );
-        }
-    }
+
+					if (!IsNight && t >= .85f)
+					{
+						IsNight = true;
+						var lights = GameObject.FindGameObjectsWithTag("Light");
+						foreach (var l in lights)
+						{
+							if (l.TryGetComponent<Light2D>(out var lightComponent))
+							{
+								lightComponent.enabled = true;
+							}
+						}
+					}
+				})
+			);
+
+			seq.Join
+			(
+				DOVirtual.Color(Camera.main.backgroundColor, seaNightColor, switchNightProgressDuration,
+					t => { Camera.main.backgroundColor = t; })
+			);
+		}
+	}
 }
